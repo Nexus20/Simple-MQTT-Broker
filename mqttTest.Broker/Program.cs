724d38a56568e9aa06718fbab522bf4a687707a4
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2022-12-19 23:34:42 +0200

    Add project files.

---
+++ mqttTest.Broker/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Server;
+
+namespace mqttTest.Broker
+{
+    internal class Program
+    {
+        private static int MessageCounter { get; set; }
+        
+        private static async Task Main(string[] args)
+        {
+            var brokerIp = IPAddress.Parse("192.168.251.144");
+            //var brokerIp = IPAddress.Parse("192.168.0.111");
+
+
+            // Create the options for our MQTT Broker
+            var options = new MqttServerOptionsBuilder()
+                // set endpoint to localhost
+                .WithDefaultEndpointBoundIPAddress(brokerIp)
+                // port used will be 707
+                .WithDefaultEndpointPort(1883)
+                // handler for new connections
+                .WithConnectionValidator(OnNewConnection)
+                // handler for new messages
+                .WithApplicationMessageInterceptor(OnNewMessage);
+
+            // creates a new mqtt server     
+            var mqttServer = new MqttFactory().CreateMqttServer();
+
+            // start the server with options  
+            await mqttServer.StartAsync(options.Build());
+
+            // keep application running until user press a key
+            Console.ReadLine();
+        }
+
+        private static void OnNewConnection(MqttConnectionValidatorContext context)
+        {
+            Console.WriteLine($"New connection: ClientId = {context.ClientId}, Endpoint = {context.Endpoint}");
+        }
+
+        private static void OnNewMessage(MqttApplicationMessageInterceptorContext context)
+        {
+            var payload = context.ApplicationMessage?.Payload == null
+                ? null
+                : Encoding.UTF8.GetString(context.ApplicationMessage?.Payload);
+
+            MessageCounter++;
+
+            Console.WriteLine($"MessageId: {MessageCounter} - TimeStamp: {DateTime.Now} -- Message: ClientId = {context.ClientId}, Topic = {context.ApplicationMessage?.Topic}, Payload = {Encoding.UTF8.GetString(context.ApplicationMessage?.Payload)}, QoS = {context.ApplicationMessage?.QualityOfServiceLevel}, Retain-Flag = {context.ApplicationMessage?.Retain}");
+        }
+    }
+}
---
+++ mqttTest.Client/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Options;
+using MQTTnet.Extensions.ManagedClient;
+using Newtonsoft.Json;
+using Serilog;
+
+namespace mqttTest.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates a new client
+            var builder = new MqttClientOptionsBuilder()
+                .WithClientId("Dev.To")
+                .WithTcpServer("localhost", 44356);
+
+            // Create client options objects
+            var options = new ManagedMqttClientOptionsBuilder()
+                .WithAutoReconnectDelay(TimeSpan.FromSeconds(60))
+                .WithClientOptions(builder.Build())
+                .Build();
+
+            // Creates the client object
+            var mqttClient = new MqttFactory().CreateManagedMqttClient();
+
+            // Set up handlers
+            mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnConnected);
+            mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(OnDisconnected);
+            mqttClient.ConnectingFailedHandler = new ConnectingFailedHandlerDelegate(OnConnectingFailed);
+
+            // Starts a connection with the Broker
+            mqttClient.StartAsync(options).GetAwaiter().GetResult();
+
+            // Send a new message to the broker every second
+            while (true)
+            {
+                var json = JsonConvert.SerializeObject(new { message = "Heyo :)", sent = DateTimeOffset.UtcNow });
+                mqttClient.PublishAsync("dev.to/topic/json", json);
+
+                Task.Delay(1000).GetAwaiter().GetResult();
+            }
+        }
+
+        private static void OnConnected(MqttClientConnectedEventArgs obj)
+        {
+            Log.Logger.Information("Successfully connected");
+        }
+
+        private static void OnConnectingFailed(ManagedProcessFailedEventArgs obj)
+        {
+            Log.Logger.Warning("Couldn't connect to broker");
+        }
+
+        private static void OnDisconnected(MqttClientDisconnectedEventArgs obj)
+        {
+            Log.Logger.Information("Successfully disconnected");
+        }
+
+    }
+}
